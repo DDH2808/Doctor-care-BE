2b38a3d38acb56abe1b970a799981f0cdcd67b73
DDH2808 <daodinhhoan2002@gmail.com>
2024-09-15 22:25:40 +0700

    CRUD React 5 - Thêm Mới Người Dùng (CREATE)

--- src/services/userService.js
+++ src/services/userService.js
@@ -115,24 +115,25 @@ const createNewUser = (data) => {
       if (check === true) {
         resolve({
           errCode: 1,
-          message: "Your email is already in used, plz try another email!",
+          errMessage: "Your email is already in used, plz try another email!",
+        });
+      } else {
+        const hashPassWordFromBcrypt = await hashUserPassword(data.password);
+        await db.User.create({
+          email: data.email,
+          password: hashPassWordFromBcrypt,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          address: data.address,
+          phonenumber: data.phonenumber,
+          gender: data.gender === "1" ? true : false,
+          roleId: data.roleId,
+        });
+        resolve({
+          errCode: 0,
+          message: "OK",
         });
       }
-      const hashPassWordFromBcrypt = await hashUserPassword(data.password);
-      await db.User.create({
-        email: data.email,
-        password: hashPassWordFromBcrypt,
-        firstName: data.firstName,
-        lastName: data.lastName,
-        address: data.address,
-        phonenumber: data.phonenumber,
-        gender: data.gender === "1" ? true : false,
-        roleId: data.roleId,
-      });
-      resolve({
-        errCode: 0,
-        message: "OK",
-      });
     } catch (error) {
       reject(error);
     }

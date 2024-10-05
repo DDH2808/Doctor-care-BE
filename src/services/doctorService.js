16a9716e82d0b702584fd969f3ed8af58f7388f1
DDH2808 <daodinhhoan2002@gmail.com>
2024-10-05 10:47:38 +0700

    Hoàn Thiện Chức Năng Đặt Lịch Khám Bệnh Cho Bệnh Nhân

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -386,7 +386,7 @@ const getProfileDoctorByIdService = (inputId) => {
           nest: true,
         });
         if (data && data.image) {
-          data.image = new Buffer(data.image, "base64").toString("binary");
+          data.image = Buffer.from(data.image, "base64").toString("binary");
         }
         if (!data) data = {};
         resolve({
